90a810804f5f21ee409db5932791a3214819403f
hdonovan <hdonovan@waveware.com>
2022-12-07 19:14:14 -0800

    can add multiple creatures at a time, and delete

--- DmBuddyMvc/wwwroot/js/encounter.js
+++ DmBuddyMvc/wwwroot/js/encounter.js
@@ -3,12 +3,17 @@ var dmb;
     var encounter;
     (function (encounter) {
         class Creature {
+            GetHP() {
+                return this.CurrentHP.toString() + "/" + this.MaxHP.toString();
+            }
         }
         const initiativeIndex = 0;
         const nameIndex = 1;
         const hpIndex = 2;
         const damageInputIndex = 3;
         const idIndex = 4;
+        const deleteIndex = 5;
+        const initiativeRegex = /\d+\.?\d?/g;
         function Init() {
             creatures = [];
             id = 0;
@@ -21,15 +26,45 @@ var dmb;
                     nameInput.focus();
                 }, 500);
             });
+            $("#addCreatureModal").keyup(function (e) {
+                if (e.keyCode === 13) {
+                    AddCreaturesAndResetForm();
+                }
+            });
         }
         encounter.Init = Init;
-        function AddCreature() {
-            let creature = new Creature();
-            creature.Id = id++;
-            creature.Name = nameInput.val();
-            creature.AC = acInput.val();
-            creature.HP = hpInput.val();
-            creature.Initiative = initiativeInput.val();
+        function AddCreaturesAndResetForm() {
+            AddCreatures();
+            ClearCreatureForm();
+        }
+        encounter.AddCreaturesAndResetForm = AddCreaturesAndResetForm;
+        function AddCreatures() {
+            //let initiatives = initiativeInput.val().toString().split(",");
+            let initiatives = initiativeInput.val().toString().match(initiativeRegex);
+            if (initiatives.length < 1)
+                return;
+            //if this creature name already exists in the table, add a ' - 1'...
+            let nameCount = 0;
+            let creaturesWithSameName = creatures.filter(c => c.Name == nameInput.val());
+            creaturesWithSameName.forEach(c => { if (Number(nameCount) < Number(c.NameCount))
+                nameCount = c.NameCount; });
+            let name = nameInput.val();
+            let ac = Number(acInput.val());
+            let hp = Number(hpInput.val());
+            for (let i = 0; i < initiatives.length; i++) {
+                let creature = new Creature();
+                creature.Name = name;
+                creature.Id = id++;
+                creature.NameCount = ++nameCount;
+                creature.Initiative = Number(initiatives[i]);
+                creature.AC = ac;
+                creature.CurrentHP = hp;
+                creature.MaxHP = hp;
+                AddCreature(creature);
+            }
+        }
+        encounter.AddCreatures = AddCreatures;
+        function AddCreature(creature) {
             var table = document.getElementById("initiativeTable");
             if (creatures.length == 0) {
                 InsertCreatureIntoTableAtIndex(creature, table, 0);
@@ -49,29 +84,32 @@ var dmb;
             creatures.splice(index, 0, creature);
             let row = table.getElementsByTagName('tbody')[0].insertRow(index);
             row.insertCell(initiativeIndex).innerHTML = creature.Initiative.toString();
-            row.insertCell(nameIndex).innerHTML = creature.Name;
-            row.insertCell(hpIndex).innerHTML = creature.HP.toString();
-            row.insertCell(damageInputIndex).innerHTML = "<button type='button' class='btn btn-danger' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ", \"-\")'>-</button>" +
-                "<input style='width:40%' type='number' id='" + creature.Id + "_DamageOrHealAmount'/>" +
-                "<button type='button' class='btn btn-success' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ")'>+</button>";
+            row.insertCell(nameIndex).innerHTML = creature.NameCount > 1 ? creature.Name + " - " + creature.NameCount : creature.Name;
+            row.insertCell(hpIndex).innerHTML = creature.GetHP();
+            row.insertCell(damageInputIndex).innerHTML = "<div class='row text-center' style='width:160px'><div class='col text-center' style='width:30px'><button type='button' class='btn btn-danger' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ", \"-\")'>-</button></div>" +
+                "<div class='col text-center' style='width:100px'><input style='width:100%' type='text' id='" + creature.Id + "_DamageOrHealAmount'/></div>" +
+                "<div class='col text-center' style='width:30px'><button type='button' class='btn btn-success' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ")'>+</button></div></div>";
             row.insertCell(idIndex).outerHTML = "<td style='display:none'>" + creature.Id + "</td>";
+            row.insertCell(deleteIndex).innerHTML = "<button type='button' class='btn btn-outline-danger' onclick='dmb.encounter.RemoveFromInitiative(" + creature.Id + ")'><i class='bi bi-trash'></i></button>";
         }
         function HealCreatureFromId(id, sign = "+") {
-            console.log("doing health: " + id + " " + sign);
             let amount = Number($("#" + id + "_DamageOrHealAmount").val());
             if (sign === "-")
                 amount = -amount;
             let creature = creatures.find(c => c.Id == id);
-            creature.HP = Number(creature.HP) + Number(amount);
+            creature.CurrentHP = Number(creature.CurrentHP) + Number(amount);
+            let creatureIndex = creatures.indexOf(creature);
             let rows = document.getElementById("initiativeTable").getElementsByTagName('tbody')[0].rows;
-            for (let i = 0; i < rows.length; i++) {
-                if (rows[i].cells[idIndex].innerHTML == id.toString()) {
-                    rows[i].cells[hpIndex].innerHTML = creature.HP.toString();
-                    return;
-                }
-            }
+            rows[creatureIndex].cells[hpIndex].innerHTML = creature.GetHP();
         }
         encounter.HealCreatureFromId = HealCreatureFromId;
+        function RemoveFromInitiative(id) {
+            let table = document.getElementById("initiativeTable").getElementsByTagName('tbody')[0];
+            let deleteIndex = creatures.indexOf(creatures.find(c => c.Id == id));
+            table.deleteRow(deleteIndex);
+            creatures.splice(deleteIndex, 1);
+        }
+        encounter.RemoveFromInitiative = RemoveFromInitiative;
         function ClearCreatureForm() {
             nameInput.val("");
             acInput.val("");
--- DmBuddyMvc/wwwroot/js/encounter.ts
+++ DmBuddyMvc/wwwroot/js/encounter.ts
@@ -3,9 +3,16 @@
     class Creature {
         Id: number;
         Name: string;
+        NameCount: number;
         AC: number;
-        HP: number;
+        CurrentHP: number;
+        MaxHP: number;
         Initiative: number;
+        Notes: string;
+
+        GetHP(): string {
+            return this.CurrentHP.toString() + "/" + this.MaxHP.toString();
+        }
     }
 
     const initiativeIndex: number = 0;
@@ -13,6 +20,9 @@
     const hpIndex: number = 2;
     const damageInputIndex: number = 3;
     const idIndex: number = 4;
+    const deleteIndex: number = 5;
+
+    const initiativeRegex = /\d+\.?\d?/g;
 
     declare let creatures: Creature[];
     declare let id: number;
@@ -35,17 +45,49 @@
                 nameInput.focus();
             }, 500);
         });
+
+        $("#addCreatureModal").keyup(function (e) {
+            if (e.keyCode === 13) {
+                AddCreaturesAndResetForm();
+            }
+        });
     }
 
-    export function AddCreature(): void {
-        let creature = new Creature();
-        creature.Id = id++;
+    export function AddCreaturesAndResetForm(): void {
+        AddCreatures();
+        ClearCreatureForm();
+    }
 
-        creature.Name = nameInput.val() as string;
-        creature.AC = acInput.val() as number;
-        creature.HP = hpInput.val() as number;
-        creature.Initiative = initiativeInput.val() as number;
+    export function AddCreatures(): void {
+        //let initiatives = initiativeInput.val().toString().split(",");
+        let initiatives = initiativeInput.val().toString().match(initiativeRegex);
+        if (initiatives.length < 1)
+            return;
 
+        //if this creature name already exists in the table, add a ' - 1'...
+        let nameCount = 0;
+        let creaturesWithSameName = creatures.filter(c => c.Name == nameInput.val() as string);
+        creaturesWithSameName.forEach(c => { if (Number(nameCount) < Number(c.NameCount)) nameCount = c.NameCount; })
+
+        let name = nameInput.val() as string;
+        let ac = Number(acInput.val());
+        let hp = Number(hpInput.val());
+
+        for (let i = 0; i < initiatives.length; i++) {
+            let creature = new Creature();
+            creature.Name = name;
+            creature.Id = id++;
+            creature.NameCount = ++nameCount;
+            creature.Initiative = Number(initiatives[i]);
+            creature.AC = ac;
+            creature.CurrentHP = hp;
+            creature.MaxHP = hp;
+
+            AddCreature(creature);
+        }
+    }
+
+    export function AddCreature(creature: Creature): void {
         var table = document.getElementById("initiativeTable") as HTMLTableElement;
 
         if (creatures.length == 0) {
@@ -70,31 +112,33 @@
 
         let row = table.getElementsByTagName('tbody')[0].insertRow(index);
         row.insertCell(initiativeIndex).innerHTML = creature.Initiative.toString();
-        row.insertCell(nameIndex).innerHTML = creature.Name;
-        row.insertCell(hpIndex).innerHTML = creature.HP.toString();
-        row.insertCell(damageInputIndex).innerHTML = "<button type='button' class='btn btn-danger' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ", \"-\")'>-</button>" +
-            "<input style='width:40%' type='number' id='" + creature.Id + "_DamageOrHealAmount'/>" +
-            "<button type='button' class='btn btn-success' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ")'>+</button>";
+        row.insertCell(nameIndex).innerHTML = creature.NameCount > 1 ? creature.Name + " - " + creature.NameCount : creature.Name;
+        row.insertCell(hpIndex).innerHTML = creature.GetHP();
+        row.insertCell(damageInputIndex).innerHTML = "<div class='row text-center' style='width:160px'><div class='col text-center' style='width:30px'><button type='button' class='btn btn-danger' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ", \"-\")'>-</button></div>" +
+            "<div class='col text-center' style='width:100px'><input style='width:100%' type='text' id='" + creature.Id + "_DamageOrHealAmount'/></div>" +
+            "<div class='col text-center' style='width:30px'><button type='button' class='btn btn-success' onclick='dmb.encounter.HealCreatureFromId(" + creature.Id + ")'>+</button></div></div>";
         row.insertCell(idIndex).outerHTML = "<td style='display:none'>" + creature.Id + "</td>";
+        row.insertCell(deleteIndex).innerHTML = "<button type='button' class='btn btn-outline-danger' onclick='dmb.encounter.RemoveFromInitiative(" + creature.Id + ")'><i class='bi bi-trash'></i></button>";
     }
 
-    export function HealCreatureFromId(id: number, sign: string = "+"): void {
-        console.log("doing health: " + id + " " + sign);
-        
+    export function HealCreatureFromId(id: number, sign: string = "+"): void {        
         let amount: number = Number($("#" + id + "_DamageOrHealAmount").val());
         if (sign === "-")
             amount = -amount;
 
         let creature = creatures.find(c => c.Id == id);
-        creature.HP = Number(creature.HP)+Number(amount);
+        creature.CurrentHP = Number(creature.CurrentHP) + Number(amount);
 
+        let creatureIndex = creatures.indexOf(creature);
         let rows = document.getElementById("initiativeTable").getElementsByTagName('tbody')[0].rows;
-        for (let i = 0; i < rows.length; i++) {
-            if (rows[i].cells[idIndex].innerHTML == id.toString()) {
-                rows[i].cells[hpIndex].innerHTML = creature.HP.toString();
-                return;
-            }
-        }
+        rows[creatureIndex].cells[hpIndex].innerHTML = creature.GetHP();
+    }
+
+    export function RemoveFromInitiative(id: number) {
+        let table = document.getElementById("initiativeTable").getElementsByTagName('tbody')[0];
+        let deleteIndex = creatures.indexOf(creatures.find(c => c.Id == id));
+        table.deleteRow(deleteIndex);
+        creatures.splice(deleteIndex, 1);
     }
 
     export function ClearCreatureForm(): void {
