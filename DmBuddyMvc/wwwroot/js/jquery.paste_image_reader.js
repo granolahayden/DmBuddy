1d0ba1a1a29ea44796e55bd140f0b2ccec6f2fb4
hdonovan <hdonovan@waveware.com>
2023-01-04 13:09:35 -0800

    all done i think

--- DmBuddyMvc/wwwroot/js/jquery.paste_image_reader.js
+++ DmBuddyMvc/wwwroot/js/jquery.paste_image_reader.js
@@ -63,91 +63,16 @@ $("html").pasteImageReader(function (results) {
     }
     var dataURL, filename;
     filename = results.filename, dataURL = results.dataURL;
+    ImagePaste(dataURL);
+});
+
+function ImagePaste(dataurl) {
     let creaturePicture = $("#creaturePictureInput");
     let pictureOverlay = $("#creaturePictureOverlay");
 
-    creaturePicture.attr("src", dataURL);
-
+    creaturePicture.attr("src", dataurl);
     creaturePicture.show();
     pictureOverlay.hide();
-    
-});
 
-
-
-//var dataURL, filename;
-//$("html").pasteImageReader(function (results) {
-//	filename = results.filename, dataURL = results.dataURL;
-//	$data.text(dataURL);
-//	$size.val(results.file.size);
-//	$type.val(results.file.type);
-//	var img = document.createElement("img");
-//	img.src = dataURL;
-//	var w = img.width;
-//	var h = img.height;
-//	$width.val(w);
-//	$height.val(h);
-//	return $(".active")
-//		.css({
-//			backgroundImage: "url(" + dataURL + ")"
-//		})
-//		.data({ width: w, height: h });
-//});
-
-//var $data, $size, $type, $width, $height;
-//$(function () {
-//	$data = $(".data");
-//	$size = $(".size");
-//	$type = $(".type");
-//	$width = $("#width");
-//	$height = $("#height");
-//	$(".target").on("click", function () {
-//		var $this = $(this);
-//		var bi = $this.css("background-image");
-//		if (bi != "none") {
-//			$data.text(bi.substr(4, bi.length - 6));
-//		}
-
-//		$(".active").removeClass("active");
-//		$this.addClass("active");
-
-//		$this.toggleClass("contain");
-
-//		$width.val($this.data("width"));
-//		$height.val($this.data("height"));
-//		if ($this.hasClass("contain")) {
-//			$this.css({
-//				width: $this.data("width"),
-//				height: $this.data("height"),
-//				"z-index": "10"
-//			});
-//		} else {
-//			$this.css({ width: "", height: "", "z-index": "" });
-//		}
-//	});
-//});
-
-//function copy(text) {
-//	var t = document.getElementById("base64");
-//	t.select();
-//	try {
-//		var successful = document.execCommand("copy");
-//		var msg = successful ? "successfully" : "unsuccessfully";
-//		alert("Base64 data coppied " + msg + " to clipboard");
-//	} catch (err) {
-//		alert("Unable to copy text");
-//	}
-//}
-
-
-//function copyMDImage() {
-//	var md = document.getElementById('base64MD');
-//	md.select();
-//	try {
-//		var successful = document.execCommand("copy");
-//		var msg = successful ? "successfully" : "unsuccessfully";
-//		alert("Markdown Base64 data coppied " + msg + " to clipboard");
-//	} catch (err) {
-//		alert("Unable to copy text");
-//	}
-//}
+    $("#creaturePictureFileInput").val('');
+}
--- DmBuddyMvc/wwwroot/js/premiumEncounter.ts
+++ DmBuddyMvc/wwwroot/js/premiumEncounter.ts
@@ -2,6 +2,8 @@
 {
     let creaturePictureInput: JQuery<HTMLElement>;
     let creaturePictureDisplay: JQuery<HTMLElement>;
+    let creaturePictureFileInput: JQuery<HTMLElement>;
+    let creaturePictureInputOverlay: JQuery<HTMLElement>;
 
     export var IsPremium: boolean;
 
@@ -12,10 +14,30 @@
             return;
 
         creaturePictureDisplay = $("#creaturePictureDisplay");
+        creaturePictureFileInput = $("#creaturePictureFileInput");
+        creaturePictureInputOverlay = $("#creaturePictureOverlay");
+
+        creaturePictureFileInput.on('input', function (event: JQueryEventObject) {
+            const input: HTMLInputElement = document.querySelector('input[type=file]');
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                // convert image file to base64 string
+                creaturePictureInput.attr("src", reader.result.toString());
+                creaturePictureInput.show();
+                creaturePictureInputOverlay.hide();
+            }, false);
+            if (input) {
+                reader.readAsDataURL(input.files[0]);
+            }
+        });
     }
 
     function GetIsPremium(): void {
         IsPremium = creaturePictureInput.val() != null;
+    }
+
+    export function SetPictureData(pictureData: string): void {
+        creaturePictureDisplay.attr("src", pictureData);
     }
 
     export function GetPictureValue(): string {
@@ -24,11 +46,13 @@
 
     export function ClearPictureInput(): void {
         creaturePictureInput.attr("src", "");
+        creaturePictureInput.hide();
+        creaturePictureInputOverlay.show();
+        creaturePictureFileInput.val('');
     }
 
-    export function SetPictureData(pictureData: string): void {
-        creaturePictureDisplay.attr("src", pictureData);
-    }
+    
+
 }
 
 $(document).ready(function (): void {
